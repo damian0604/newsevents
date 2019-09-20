12a497aae5e94f987932c9b7a5aea08c793a715f
damian0604 <d.c.trilling@uva.nl>
2019-09-20 18:25:03 +0200

    makde pkl2net work for multiple models

--- src/data-processing/030-pkl2net.py
+++ src/data-processing/030-pkl2net.py
@@ -3,20 +3,28 @@
 import networkx as nx
 from tqdm import tqdm
 from glob import glob
-from os.path import basename
+import os
 import pickle
 
-filenames = glob('../../data/raw-private/softcosine_output/*.pkl')
 
-for fn in tqdm(filenames):
-    with open(fn, mode='rb') as fi:
-        df = pickle.load(fi)
-        G = nx.Graph()
-        # change int to str (necessary for pajek format)
-        df['similarity'] = df['similarity'].apply(str)
-        # change column name to 'weights' to faciliate later analysis
-        df.rename({'similarity':'weight'}, axis=1, inplace=True) 
-        # notes and weights from dataframe
-        G = nx.from_pandas_edgelist(df, source='source', target='target', edge_attr='weight')
-        # write to pajek
-        nx.write_pajek(G, '../../data/intermediate/'+basename(fn)[:-3]+'net')
+models = [('softcosine', glob('../../data/raw-private/softcosine_output/*.pkl')),
+          ('cosine', glob('../../data/raw-private/cosine_output/*.pkl'))]
+
+for modelname, filenames in models:
+    print('Processing output of the {} model...\n\n\n'.format(modelname))
+    for fn in tqdm(filenames):
+        with open(fn, mode='rb') as fi:
+            df = pickle.load(fi)
+            G = nx.Graph()
+            # change int to str (necessary for pajek format)
+            df['similarity'] = df['similarity'].apply(str)
+            # change column name to 'weights' to faciliate later analysis
+            df.rename({'similarity':'weight'}, axis=1, inplace=True) 
+            # notes and weights from dataframe
+            G = nx.from_pandas_edgelist(df, source='source', target='target', edge_attr='weight')
+            # write to pajek
+            path = '../../data/intermediate/{}/'.format(modelname)
+            if not os.path.exists(path):
+                os.mkdir(path)
+            nx.write_pajek(G, path+os.path.basename(fn)[:-3]+'net')
+
